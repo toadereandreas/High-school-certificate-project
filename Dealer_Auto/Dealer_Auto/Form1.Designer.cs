b055fddcac79aa5a77d92f761cf35806f01408bc
toadereandreas <57907218+toadereandreas@users.noreply.github.com>
2020-03-06 13:20:15 +0200

    Add files via upload

---
+++ Dealer_Auto/Dealer_Auto/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Dealer_Auto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_Utilizator = new System.Windows.Forms.Label();
+            this.textBox_Utilizator = new System.Windows.Forms.TextBox();
+            this.textBox_Parola = new System.Windows.Forms.TextBox();
+            this.label_Parola = new System.Windows.Forms.Label();
+            this.button_Logare = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_Utilizator
+            // 
+            this.label_Utilizator.AutoSize = true;
+            this.label_Utilizator.Location = new System.Drawing.Point(94, 64);
+            this.label_Utilizator.Name = "label_Utilizator";
+            this.label_Utilizator.Size = new System.Drawing.Size(79, 20);
+            this.label_Utilizator.TabIndex = 0;
+            this.label_Utilizator.Text = "Utilizator :";
+            // 
+            // textBox_Utilizator
+            // 
+            this.textBox_Utilizator.Location = new System.Drawing.Point(179, 64);
+            this.textBox_Utilizator.Name = "textBox_Utilizator";
+            this.textBox_Utilizator.Size = new System.Drawing.Size(317, 26);
+            this.textBox_Utilizator.TabIndex = 1;
+            // 
+            // textBox_Parola
+            // 
+            this.textBox_Parola.Location = new System.Drawing.Point(179, 105);
+            this.textBox_Parola.Name = "textBox_Parola";
+            this.textBox_Parola.Size = new System.Drawing.Size(317, 26);
+            this.textBox_Parola.TabIndex = 3;
+            // 
+            // label_Parola
+            // 
+            this.label_Parola.AutoSize = true;
+            this.label_Parola.Location = new System.Drawing.Point(94, 105);
+            this.label_Parola.Name = "label_Parola";
+            this.label_Parola.Size = new System.Drawing.Size(62, 20);
+            this.label_Parola.TabIndex = 2;
+            this.label_Parola.Text = "Parola :";
+            // 
+            // button_Logare
+            // 
+            this.button_Logare.Location = new System.Drawing.Point(331, 171);
+            this.button_Logare.Name = "button_Logare";
+            this.button_Logare.Size = new System.Drawing.Size(165, 38);
+            this.button_Logare.TabIndex = 4;
+            this.button_Logare.Text = "Logheaza-te";
+            this.button_Logare.UseVisualStyleBackColor = true;
+            this.button_Logare.Click += new System.EventHandler(this.button_Logare_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Logare);
+            this.Controls.Add(this.textBox_Parola);
+            this.Controls.Add(this.label_Parola);
+            this.Controls.Add(this.textBox_Utilizator);
+            this.Controls.Add(this.label_Utilizator);
+            this.Name = "Form1";
+            this.Text = "Fereastra Logare";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Utilizator;
+        private System.Windows.Forms.TextBox textBox_Utilizator;
+        private System.Windows.Forms.TextBox textBox_Parola;
+        private System.Windows.Forms.Label label_Parola;
+        private System.Windows.Forms.Button button_Logare;
+    }
+}
+
---
+++ Dealer_Auto/Dealer_Auto/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Dealer_Auto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection conexiune;
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            string adresa_Bazei_De_Date = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=BazaDeDate.accdb;Persist Security Info=True";
+            conexiune = new OleDbConnection(adresa_Bazei_De_Date);
+            try
+            {
+                conexiune.Open();
+            }
+            catch( Exception exceptie )
+            {
+                MessageBox.Show(exceptie.Message);
+            }
+
+        }
+
+        private void button_Logare_Click(object sender, EventArgs e)
+        {
+            string utilizator = textBox_Utilizator.Text;
+            string parola = textBox_Parola.Text;
+            if( utilizator != "" && parola != "" )
+            {
+                string text_Comanda = "SELECT * FROM [Tabel_Useri] WHERE Username='" + utilizator + "' AND Parola ='" + parola + "'";
+                OleDbCommand comanda = new OleDbCommand(text_Comanda, conexiune);
+                OleDbDataReader cititor_Date = comanda.ExecuteReader();
+                if (cititor_Date.HasRows)
+                {
+                    cititor_Date.Read();
+                    MessageBox.Show(cititor_Date[4].ToString() + " " + cititor_Date[6].ToString());
+
+                }
+                else
+                    MessageBox.Show("Nu ati introdus datele !");
+           
+            }
+            else
+                MessageBox.Show("Nu ati introdus datele !");
+        }
+    }
+}
